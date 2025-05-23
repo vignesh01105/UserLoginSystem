a481c469b11d7e754d3a463924ed5f6b18447f0b
Vignesh Kumar Mahadevan <vignesh01105@gmail.com>
2025-05-23 11:09:26 +0530

    Update UserLoginSystemApplication.java
    Add Exclude in Application for not configured DB

--- src/main/java/com/example/userLoginSystem/UserLoginSystemApplication.java
+++ src/main/java/com/example/userLoginSystem/UserLoginSystemApplication.java
@@ -2,8 +2,9 @@ package com.example.userLoginSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class UserLoginSystemApplication {
 
 	public static void main(String[] args) {
