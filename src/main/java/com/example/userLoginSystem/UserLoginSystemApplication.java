2c37c5b6184b60f53901feef2f74de2c894a476d
Vignesh Kumar Mahadevan <vignesh01105@gmail.com>
2025-05-23 08:51:15 +0530

    Foundation
    Add Dependencies for Project

---
+++ src/main/java/com/example/userLoginSystem/UserLoginSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.userLoginSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userLoginSystem/UserLoginSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userLoginSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
